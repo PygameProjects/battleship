e6da2d3c99906520af57cd7263ad6b2c8de5a4c1
mauricio bellon <mauriciobellon3@gmail.com>
2013-02-21 17:29:24 -0200

    Create battleship.py

---
+++ battleship.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#THIS IS PROJECT ONE, WE'LL FIRST TRY TO MAKE A BATTLESHIP GAME,
+#PLEASE MAKE USE OF COMMENT WHEN PULLING REQUESTS
+
+
+# Importing pygame modules
+import pygame
+from pygame.locals import *
+# and other modules needed
+
+# ----------------------------------------------
+# Set variables, like screen width and height 
+
+# ----------------------------------------------
+# Clases and Functions used
+
+# ----------------------------------------------
+
+def main():
+    pygame.init()
+    # Here goes the principal function of the game
+    # This is the soul, the loop.
+
+if __name__ == "__main__": #This calls the game loop
+    main()
