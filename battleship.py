2797818b7fc29d1877066bf429c5a212bc0c4ae2
Wade Okumura <theprofessional9@gmail.com>
2013-02-23 19:06:37 -1000

    added gameboard

--- battleship.py
+++ battleship.py
@@ -23,12 +23,17 @@ TEXT_HEIGHT = 25
 TEXT_LEFT_POSN = 10
 BOARDWIDTH = 12
 BOARDHEIGHT = 12
+DISPLAYWIDTH = 200
+XMARGIN = int((WINDOWWIDTH - (BOARDWIDTH * TILESIZE) - (DISPLAYWIDTH + 20)) / 2)
+YMARGIN = int((WINDOWHEIGHT - (BOARDHEIGHT * TILESIZE)) / 2)
 
 BLACK   = (  0,   0,   0)
 WHITE   = (255, 255, 255)
 GREEN   = (  0, 204,   0)
 GRAY    = ( 60,  60,  60)
 BLUE    = (  0,  50, 255)
+YELLOW  = (255, 255,   0)
+DARKGRAY =( 40,  40,  40)
 
 BGCOLOR = GRAY
 BUTTONCOLOR = GREEN
@@ -36,6 +41,7 @@ TEXTCOLOR = WHITE
 TILECOLOR = GREEN
 BORDERCOLOR = BLUE
 TEXTSHADOWCOLOR = BLUE
+SHIPCOLOR = YELLOW
 
 
 def main():
@@ -63,10 +69,13 @@ def main():
         
         
 def run_game():
+    revealed_boxes = generate_revealed_boxes(False)
+    
     while True:
         DISPLAYSURF.fill(BGCOLOR)        
         DISPLAYSURF.blit(HELP_SURF, HELP_RECT)
         DISPLAYSURF.blit(NEW_SURF, NEW_RECT)
+        draw_board(revealed_boxes)
 
         check_for_quit()
         for event in pygame.event.get():
@@ -80,12 +89,40 @@ def run_game():
         FPSCLOCK.tick(FPS)
 
         
+def generate_revealed_boxes(val):
+    revealed_boxes = []
+    for i in range(BOARDWIDTH):
+        revealed_boxes.append([val] * BOARDHEIGHT)
+    return revealed_boxes
+    
+    
 def check_for_quit():
     for event in pygame.event.get(QUIT):
         pygame.quit()
         sys.exit()
+
+
+def draw_board(revealed):
+    for boxx in range(BOARDWIDTH):
+        for boxy in range(BOARDHEIGHT):
+            left, top = left_top_coords_box(boxx, boxy)
+            if not revealed[boxx][boxy]:
+                pygame.draw.rect(DISPLAYSURF, TILECOLOR, (left, top, TILESIZE, TILESIZE))
+            else:
+                pygame.draw.rect(DISPLAYSURF, SHIPCOLOR, (left, top, TILESIZE, TILESIZE))
+                
+    for x in range(0, BOARDWIDTH * TILESIZE, TILESIZE):
+        pygame.draw.line(DISPLAYSURF, DARKGRAY, (x + XMARGIN, YMARGIN), (x + XMARGIN, WINDOWHEIGHT - YMARGIN))
+    for y in range(0, BOARDHEIGHT * TILESIZE, TILESIZE):
+        pygame.draw.line(DISPLAYSURF, DARKGRAY, (XMARGIN, y + YMARGIN), (WINDOWWIDTH - (DISPLAYWIDTH + 20 + XMARGIN), y + YMARGIN))
+
         
-        
+def left_top_coords_box(boxx, boxy):
+    left = boxx * TILESIZE + XMARGIN
+    top = boxy * TILESIZE + YMARGIN
+    return (left, top)
+ 
+ 
 def show_help_screen():
     # display the help screen until a button is pressed
     line1_surf, line1_rect = make_text_objs('Press a key to exit.', BASICFONT,
